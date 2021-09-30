b72a797a96a69c274b850996f35105e61feb93b6
Fabian Brash <fabianbrash@gmail.com>
2021-09-30 16:16:46 -0400

    fixed circulatingsupply null bug

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -28,7 +28,7 @@ const CryptoDetails = () => {
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
-        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+        { title: 'Price to USD', value: `$${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
         { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
         { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
         { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
@@ -40,7 +40,7 @@ const CryptoDetails = () => {
         { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
         { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
         { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+        { title: 'Circulating Supply', value: (cryptoDetails.circulatingSupply != null) ? `$ ${millify(cryptoDetails.circulatingSupply)}` : 0, icon: <ExclamationCircleOutlined /> },
       ];
 
 
