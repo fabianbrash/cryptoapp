03923d3581a0afcb0c54507d7ea37583caf8c36b
Fabian Brash <fabianbrash@gmail.com>
2021-10-04 14:52:20 -0400

    fixed build warning/error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
-{/*import icon from '../images/cryptocurrency.png';*/}
-
 
 const Navbar = () => {
     const [activeMenu, setActiveMenu] = useState(true);
