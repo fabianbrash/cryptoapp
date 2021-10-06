87087f6d0cbf894ada2551f5b893772c89691d61
Fabian Brash <fabianbrash@gmail.com>
2021-10-06 11:30:00 -0400

    added the valueStyle prop to the Statistic component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -20,11 +20,11 @@ const Homepage = () => {
         <>
         <Title level={2} className="heading">Global Crypto Stats</Title>
         <Row>
-            <Col span={12}><Statistic title="Total Cryptocurrencies" className="fb-font" value={globalStats.total} /></Col>
-            <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-            <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
-            <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
-            <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" valueStyle={{'font-family': 'Rubik'}} value={globalStats.total} /></Col>
+            <Col span={12}><Statistic title="Total Exchanges" valueStyle={{'font-family': 'Rubik'}} value={millify(globalStats.totalExchanges)} /></Col>
+            <Col span={12}><Statistic title="Total Market Cap" valueStyle={{'font-family': 'Rubik'}} value={millify(globalStats.totalMarketCap)} /></Col>
+            <Col span={12}><Statistic title="Total 24h Volume" valueStyle={{'font-family': 'Rubik'}} value={millify(globalStats.total24hVolume)} /></Col>
+            <Col span={12}><Statistic title="Total Markets" valueStyle={{'font-family': 'Rubik'}} value={millify(globalStats.totalMarkets)} /></Col>
             
         </Row>
         <div className="home-heading-container">
