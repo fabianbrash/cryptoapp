b9b71a9a6bd527fbec040034a5ce9699498b2ae5
Fabian Brash <fabianbrash@gmail.com>
2021-10-06 16:34:05 -0400

    fixed logic issue

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -45,7 +45,7 @@ const Cryptocurrencies = ({ simplified }) => {
                             >
                                 <p>Price: {millify(currency.price)}</p>
                                 <p>Market Cap: {millify(currency.marketCap)}</p>
-                                <p>Daily Change: <span style={{color: (currency.change < 1) ? "red" : "green", "font-weight": '900'}}>{millify(currency.change)}%</span></p>
+                                <p>Daily Change: <span style={{color: (currency.change < 0) ? "red" : "green", "font-weight": '900'}}>{millify(currency.change)}%</span></p>
 
                             </Card>
                         </Link>
